0625b312c8ae06ae29608a7b235ae674dd98ea6d
LeLunZ <31982496+LeLunZ@users.noreply.github.com>
2021-10-25 23:18:11 +0200

    added log output

--- src/run.py
+++ src/run.py
@@ -1,7 +1,15 @@
+import logging
+
 from Functions.config import getConfig
 from Functions.helper import export_all_tables
 from crawl_oebb import crawl, crawl_routes
 from stop_crawler import start_stop_crawler, crawl_stops
+
+logging.basicConfig(filename='./Data/aptc.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
 if __name__ == '__main__':
     try:
@@ -10,6 +18,8 @@ if __name__ == '__main__':
             crawl_routes()
             count = 1
             while count != 0:
+                print('Restarting crawling')
+                logging.debug('Restarting crawling')
                 crawl_stops()
                 count = crawl()
     except KeyError as e:
